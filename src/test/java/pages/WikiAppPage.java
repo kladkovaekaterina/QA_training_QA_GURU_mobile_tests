0db28c85cd39f89efbe15dc846cec1f8071cedec
Ekaterina Kladkova <e.kladkova@qiwi.com>
2024-05-16 17:18:35 +0300

    added allure steps

--- src/test/java/pages/WikiAppPage.java
+++ src/test/java/pages/WikiAppPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
@@ -15,28 +16,33 @@ public class WikiAppPage {
                                   textView = $(className("android.widget.TextView"));
     private final ElementsCollection titlesCollection = $$(id("org.wikipedia.alpha:id/page_list_item_title"));
 
+    @Step("Открытие поисковой строки")
     public WikiAppPage openSearchQueryString() {
         searchWikiString.click();
 
         return this;
     }
 
+    @Step("Ввод данных в поисковую строку")
     public WikiAppPage setSearchQuery(String value) {
         searchScrText.sendKeys(value);
 
         return this;
     }
 
+    @Step("Открытие статьи")
     public WikiAppPage openSearchedArticle(String value) {
         titlesCollection.find(text(value)).click();
 
         return this;
     }
 
+    @Step("Проверка результатов поиска")
     public void checkResultPos() {
         titlesCollection.shouldHave(sizeGreaterThan(0));
     }
 
+    @Step("Проверка результатов открытия статьи")
     public void checkPageDidntOpenWithText(String value) {
         textView.shouldNotHave(text(value));
     }
